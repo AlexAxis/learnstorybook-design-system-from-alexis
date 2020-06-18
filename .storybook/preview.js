22692912f684341c5818092293df34cecbae2811
Alexis Sousa <alexis-sousa@hotmail.com>
2020-06-18 13:25:19 +0200

    Add addon Accessibility

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { addDecorator } from "@storybook/react";
 import { GlobalStyle } from "../src/shared/global";
+import { withA11y } from "@storybook/addon-a11y";
 
 //--- we can see how this change affect the code in the story Avatar-Initials
 //--- the font of the letters is different
 
+addDecorator(withA11y);
 addDecorator((story) => (
   <>
     <GlobalStyle />
